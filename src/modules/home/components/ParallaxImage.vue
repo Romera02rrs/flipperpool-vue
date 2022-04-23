56801fb78137d1f9c8fc3b77b8a3c925bb257248
= <76781930+Romera02rrs@users.noreply.github.com>
2022-04-23 18:34:38 +0200

    Prueba de texto en el comoponente parallax

--- src/modules/home/components/ParallaxImage.vue
+++ src/modules/home/components/ParallaxImage.vue
@@ -5,12 +5,16 @@
       sectionSubTitle="La mayor calidad y durabilidad a su alcance"
     />
     <div class="imagen">
+      <div class="contenido">
+        <h5>Construímos mas de 30<br> piscinas anuales</h5>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 import HeadlineGroup from "@/components/HeadlineGroup.vue";
+import Header from '@/components/Header.vue';
 export default {
   components: { HeadlineGroup },
 };
@@ -20,9 +24,30 @@ export default {
 
 .imagen {
   with: 100%;
-  height: 250px;
-  background: center fixed url('../assets/terminado.jpg');
+  height: 300px;
+  background: center fixed url('../assets/02.jpg');
   filter: brightness(90%);
+  position: relative;
+}
+
+.contenido {
+  position: absolute;
+  width: 75%;
+  height: 50%;
+  background-color: #8cccffaa;
+  z-index: 2;
+  left: calc(50% - (75% / 2));
+  top: calc(50% - (50% / 2));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h5 {
+  text-align: center;
+  font-size: 24px;
+  color: white;
+  margin: 15px;
 }
 
 </style>
